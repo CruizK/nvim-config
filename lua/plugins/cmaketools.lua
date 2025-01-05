b5e308784ae910de2014b9cdda89399f7b586e9d
CruizK <Cruiz.Kusman@gmail.com>
2025-01-05 12:37:46 -0600

    Updated cmake tools config

--- lua/plugins/cmaketools.lua
+++ lua/plugins/cmaketools.lua
@@ -1,43 +1,48 @@
 return {
     'Civitasv/cmake-tools.nvim',
     dependencies = { 'nvim-lua/plenary.nvim' },
-    opt = {
-        cmake_generate_options = { '-DCMAKE_EXPORT_COMPILE_COMMNADS=1' },
+    opts = {
+        cmake_command = 'cmake',
+        cmake_generate_options = { '-DCMAKE_EXPORT_COMPILE_COMMANDS=1' },
         cmake_build_options = { '-j' },
-        cmake_build_directory = "build",
+        cmake_build_directory = 'build',
+        cmake_variants_message = {
+            short = { show = true },
+            long = { show = true, max_length = 40 },
+        },
         cmake_dap_configuration = {
-            name = "cpp",
-            type = "codelldb",
-            request = "launch",
+            name = 'cpp',
+            type = 'codelldb',
+            request = 'launch',
             stopOnEntry = false,
             runInTerminal = true,
-            console = "integratedTerminal"
+            console = 'integratedTerminal',
         },
         cmake_executor = {
-            name = "quickfix",
+            name = 'quickfix',
             opts = {
-                show = "always",
-                position = "belowright",
-                size = 8,
-                encoding = "utf-8",
-                auto_close_when_success = true
-            }
+                show = 'always',
+                position = 'belowright',
+                size = 10,
+                encoding = 'utf-8',
+                auto_close_when_success = true,
+            },
         },
         cmake_runner = {
-            name = "terminal",
+            name = 'quickfix',
             opts = {
-                name = "Main Terminal",
-                prefix_name = "[CMakeTools]: ",
-                split_direction = "horizonal",
-                split_size = 8
-            }
+                show = 'always',
+                position = 'belowright',
+                size = 10,
+                encoding = 'utf-8',
+                auto_close_when_success = true,
+            },
         },
         cmake_notifications = {
             runner = { enabled = true },
             executor = { enabled = true },
-            spinner = { "⠋", "⠙", "⠹", "⠸", "⠼", "⠴", "⠦", "⠧", "⠇", "⠏" }, -- icons used for progress display
-            refresh_rate_ms = 100, -- how often to iterate icons
+            spinner = { '⠋', '⠙', '⠹', '⠸', '⠼', '⠴', '⠦', '⠧', '⠇', '⠏' },
+            refresh_rate_ms = 100,
         },
-        cmake_virtual_text_support = true
-    }
+    },
 }
